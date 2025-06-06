ba75e6508787d622650d38bffaf13870c9039eed
Joao Victor de Souza <73joao.victor@gmail.com>
2025-06-06 18:04:00 -0300

    protocolo URL alterado

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,6 +1,6 @@
 export const getApiData = async (endpoint) => {
     try {
-        const url = new URL(`http://dnc-react-api.vercel.app/files/${endpoint}`)
+        const url = new URL(`https://dnc-react-api.vercel.app/files/${endpoint}`)
         const response = await fetch(url, {
             method: 'GET',              
         })
